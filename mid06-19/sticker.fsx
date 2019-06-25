5692456e2e39287f4eb89f686024ebb79d7aff29
MASSIMO PUDDU <m.puddu5@studenti.unipi.it>
2019-06-26 00:59:18 +0200

    es 1 completato

--- mid06-19/sticker.fsx
+++ mid06-19/sticker.fsx
@@ -23,6 +23,7 @@ type Sticker() =
     member this.Image 
         with get() = image
         and set(v) = image <- v
+
     member this.OnPaint(g:Graphics) = 
         let mutable count = 0
         for i in image do
@@ -30,18 +31,24 @@ type Sticker() =
             count <- count + 1
         let mutable dimTesto = testo.Length
         let mutable posStringa = 0
-        let mutable contaRiga = 0
-        //let mutable textSize = TextRenderer.MeasureText(testo, font)
-        //printfn "%A" textSize
-        while dimTesto*int font.Size > 0 && contaRiga < 117 do
-            let mutable lenght = 9
-            if dimTesto < 10 then lenght <- dimTesto
-            let nuovo = testo.Substring( posStringa , lenght)
-            g.DrawString(nuovo,font, Brushes.Black, PointF(float32(rettangolo.X), float32(rettangolo.Y + contaRiga)))
-            posStringa <- posStringa + 9
-            dimTesto <- dimTesto - 9
-            contaRiga <- contaRiga + 13
-            //textSize <- TextRenderer.MeasureText(nuovo, font)
+        let mutable contaAltezza = rettangolo.Y + 30*count + 5
+        let mutable todraw = testo
+
+        let mutable size = TextRenderer.MeasureText(testo, font); //studio la dimensione del testo in anticipo
+
+        while size.Width <> 0 && (contaAltezza + size.Height - 5) < (rettangolo.Height + rettangolo.Y) && posStringa < testo.Length do
+
+            let mutable numlettere = 0 //numero lettere usate per disegnare
+            
+            while numlettere < (dimTesto - posStringa) && TextRenderer.MeasureText(testo.Substring( posStringa , numlettere), font).Width < rettangolo.Width - 5 do
+                numlettere <- numlettere + 1
+
+            todraw <- testo.Substring( posStringa , numlettere)
+            g.DrawString(todraw,font, Brushes.Black, PointF(float32(rettangolo.X-2), float32(contaAltezza)))
+            size <- TextRenderer.MeasureText(todraw,font)
+            contaAltezza <- contaAltezza + size.Height
+            posStringa <- posStringa + numlettere
+ 
         
 
 //------------------------------------funzioni di utilita----------------------------------
@@ -68,6 +75,9 @@ let mutable lastMousePosition = Point(-1,-1)
 type Buttons() =
     inherit LWCControl()
 
+    let mutable contaRotate = 0 //indica di quanto ha ruotato il mondo fino a questo momento
+    let sposta = 30.f //usato per indicare di quanto fare la translate
+
     let bottoni = [| new Rectangle(0,0,25,25); new Rectangle(25+5,0,25,25); new Rectangle(25*2+5*2,0,25,25); new Rectangle(25*3+5*3,0,25,25); new Rectangle(25*4+5*4,0,25,25); new Rectangle(25*5+5*5,0,25,25); new Rectangle(25*6+5*6,0,25,25); new Rectangle(25*7+5*7,0,25,25) |]
     let lettere = [| "←";"→";"↑";"↓";"+";"-";"⟲";"⟳" |]
 
@@ -80,14 +90,16 @@ type Buttons() =
             else
                 trovato <- true
                 match i with
-                | 0 -> printfn "←"
-                | 1 -> printfn "→"
-                | 2 -> printfn "↑"
-                | 3 -> printfn "↓"
-                | 4 -> printfn "+"
-                | 5 -> printfn "-"
-                | 6 -> printfn "⟲"
-                | 7 -> printfn "⟳"
+                | 0 -> printfn "←"; LWCArea.WV.TranslateW(-sposta, 0.f)
+                | 1 -> printfn "→"; LWCArea.WV.TranslateW(sposta, 0.f)
+                | 2 -> printfn "↑"; LWCArea.WV.TranslateW(0.f, -sposta)
+                | 3 -> printfn "↓"; LWCArea.WV.TranslateW(0.f, +sposta)
+                | 4 -> printfn "+"; LWCArea.WV.ScaleW(1.f*float32 1.1,1.f*float32 1.1)
+                | 5 -> printfn "-"; LWCArea.WV.ScaleW(1.f/float32 1.1,1.f/float32 1.1)
+                | 6 -> printfn "⟲";LWCArea.WV.RotateW(float32 -contaRotate); contaRotate <- (contaRotate+45)%360; LWCArea.WV.RotateW(float32 contaRotate)
+                | 7 -> printfn "⟳";LWCArea.WV.RotateW(float32 -contaRotate); contaRotate <- (contaRotate-45)%360; LWCArea.WV.RotateW(float32 contaRotate)
+                | _ -> printfn "error"
+                this.Invalidate()
 
     override this.OnPaint(e) =
         let g = e.Graphics
@@ -119,12 +131,14 @@ type Operazioni() =
                         if ofd.ShowDialog() = DialogResult.OK then
                             selectedSticker.Image <- List.append selectedSticker.Image [Image.FromFile ofd.FileName]
                             this.Invalidate()
+                    else 
+                        MessageBox.Show("Selezionare uno sticker o limite immagini per lo sticker superato", "Impossibile selezionare file", MessageBoxButtons.OK, MessageBoxIcon.Error) |> ignore
                 | 1 -> 
                     printfn "T"
                     if selectedSticker.Rettangolo.Height <> 0 && textBox.TextLength>0 then
                         selectedSticker.Testo <- textBox.Text
                         this.Invalidate()
-                | 2 -> printfn "vuoto"
+                | _ -> printfn "error"
     
 
     override this.OnPaint(e) =
@@ -140,10 +154,12 @@ type Area() =
         let risultatoricerca = trovaSticker listaSticker e.Location
         if risultatoricerca.Rettangolo.Height = 0 then //aggiungi nuovo sticker
             let st = new Sticker(Rettangolo = new Rectangle(e.Location.X,e.Location.Y,100,130))
-            listaSticker <- List.append listaSticker [ st ]
+            listaSticker <- List.append [ st ] listaSticker
             selectedSticker <- st
         else //seleziona lo steaker cliccato
             selectedSticker <- risultatoricerca
+            listaSticker <- List.append [selectedSticker] listaSticker //in modo da avere l'elemento selezionato in cima alla lista
+            listaSticker <- List.distinct listaSticker //elimino il duplicato dopo l'inserimento
         lastMousePosition <- Point( e.X - selectedSticker.Rettangolo.X, e.Y - selectedSticker.Rettangolo.Y)
         this.Invalidate()                
     
@@ -164,18 +180,119 @@ type Area() =
         if selectedSticker.Rettangolo.Height <> 0 then
             g.FillRectangle(Brushes.YellowGreen, selectedSticker.Rettangolo)
             g.DrawRectangle(Pens.Black, selectedSticker.Rettangolo)
-            selectedSticker.OnPaint g           
+            selectedSticker.OnPaint g
 
 let f = new Form(Text="prova", TopMost=true)
 f.Height <- 700
 f.Width <- 700
 f.BackColor <- Color.LightGray
+ 
+type ScrollBarX() =
+    inherit LWCControl()
+    let mutable rettOrizzontale = new Rectangle(0,0,20,20)
+    let mutable seleziona = false
+    let mutable lastPosition = new PointF(-20.f,0.f)
+    let mutable lastX = 0.f
+
+    override this.OnMouseDown(e) =
+        if checkPickCorrelationRett rettOrizzontale e.Location then
+            seleziona <- true
+            lastPosition <- PointF(float32 e.X - float32 rettOrizzontale.X, 0.f)
+
+    override this.OnMouseMove(e) =
+        if seleziona && e.Button.Equals(MouseButtons.Left) then
+            let sottr = e.Location.X - int lastPosition.X
+            let diff = float32 e.Location.X - lastX
+            if not(int(this.Width) <= (rettOrizzontale.X + rettOrizzontale.Width) && diff > 0.f) && not(rettOrizzontale.X <= 0 && diff < 0.f) then
+                rettOrizzontale <- new Rectangle(sottr, 0, 20, 20)
+                LWCArea.WV.TranslateW(diff,0.f)
+                lastX <- float32 e.Location.X
+                this.Invalidate()
+
+    override this.OnMouseUp(e) =
+        seleziona <- false
+
+    override this.OnPaint(e) =
+        let g = e.Graphics
+        g.FillRectangle(Brushes.Gray, 0.f, 0.f, this.Width, this.Height)
+        g.FillRectangle(Brushes.Black, rettOrizzontale)
+
+    override this.OnResize(e) =
+        this.ClientSize <- SizeF(float32 (f.ClientSize.Width-20), 20.f)
+        this.Position <- PointF(0.f, float32 (f.ClientSize.Height - 20))
+        if lastPosition.X = -20.f then
+            rettOrizzontale <- new Rectangle(int (this.Width/2.f),0,20,20)
+            lastPosition <- new PointF(this.Width/2.f ,0.f)
+            lastX <- lastPosition.X
+        this.Invalidate()
+        base.OnResize e
+
+type ScrollBarY() =
+    inherit LWCControl()
+    let mutable rettVerticale = new Rectangle(0,0,20,20)
+    let mutable seleziona = false
+    let mutable lastPosition = new PointF(-20.f,0.f)
+    let mutable lastY = 0.f
+
+    override this.OnMouseDown(e) =
+        if checkPickCorrelationRett rettVerticale e.Location then
+            seleziona <- true
+            lastPosition <- PointF(0.f, float32 e.Y - float32 rettVerticale.Y)
+
+    override this.OnMouseMove(e) =
+        if seleziona && e.Button.Equals(MouseButtons.Left) then
+            let sottr = e.Location.Y - int lastPosition.Y
+            let diff = float32 e.Location.Y - lastY
+            if not(int(this.Height) <= (rettVerticale.Y + rettVerticale.Height) && diff > 0.f) && not(rettVerticale.Y <= 0 && diff < 0.f) then
+                rettVerticale <- new Rectangle(0, sottr, 20, 20)
+                LWCArea.WV.TranslateW(0.f,diff)
+                lastY <- float32 e.Location.Y
+                this.Invalidate()
+
+    override this.OnMouseUp(e) =
+        seleziona <- false
+
+    override this.OnPaint(e) =
+        let g = e.Graphics
+        g.FillRectangle(Brushes.Gray, 0.f, 0.f, this.Width, this.Height)
+        g.FillRectangle(Brushes.Black, rettVerticale)
+
+    override this.OnResize(e) =
+        this.ClientSize <- SizeF(20.f, float32 (f.ClientSize.Height-20))
+        this.Position <- PointF(float32 (f.ClientSize.Width-20), 0.f)
+        if lastPosition.X = -20.f then
+            rettVerticale <- new Rectangle(0, int (this.Height/2.f),20,20)
+            lastPosition <- new PointF(0.f, this.Height/2.f)
+            lastY <- lastPosition.Y
+        
+        this.Invalidate()
+        base.OnResize e
+
+type Disegno() =
+    inherit LWCControl()
+        
+    override this.OnPaint(e) =
+        let g = e.Graphics
+        g.FillRectangle(Brushes.DimGray, 0.f, 0.f, this.Width, this.Height)
+        
+    override this.OnResize(e) =
+        this.Position <- PointF(float32 f.ClientSize.Width - 20.f, float32 f.ClientSize.Height - 20.f)
+        this.Invalidate()
+        base.OnResize e
+
+
 
 let lwcc = new LWCContainer(Dock=DockStyle.Fill)
 
-let AreaDraw = new Area(Position=PointF(100.f, 75.f),ClientSize=SizeF(float32(f.ClientSize.Width-150), float32(f.ClientSize.Height-100)))
+let AreaDraw = new Area(Position=PointF(75.f, 65.f),ClientSize=SizeF(float32(f.ClientSize.Width-150), float32(f.ClientSize.Height-100)))
 let ButtonControl = new Buttons(Position=PointF(0.f, 0.f),ClientSize=SizeF(8.f*25.f + 5.f*7.f, 25.f))
 let StickerControl = new Operazioni(Position=PointF(0.f, 30.f),ClientSize=SizeF(8.f*25.f + 5.f*7.f, 25.f))
+
+//scrollbar
+let xS = new ScrollBarX(Position=PointF(0.f, float32 (f.ClientSize.Height - 20)),ClientSize=SizeF(float32 (f.ClientSize.Width-20), 20.f))
+let yS = new ScrollBarY(Position=PointF(float32 (f.ClientSize.Width-20), 0.f),ClientSize=SizeF(20.f, float32 (f.ClientSize.Height-20)))
+let cO = new Disegno(Position=PointF(float32 f.ClientSize.Width, float32 f.ClientSize.Height - 20.f),ClientSize=SizeF(20.f, 20.f)) //sbarra tra le due scrollbar
+//------------
 
 LWCArea <- AreaDraw
 
@@ -183,6 +300,8 @@ lwcc.LWControls.Add(AreaDraw)
 lwcc.LWControls.Add(ButtonControl)
 lwcc.LWControls.Add(StickerControl)
 lwcc.Controls.Add(textBox)
-
+lwcc.LWControls.Add(xS)
+lwcc.LWControls.Add(yS)
+lwcc.LWControls.Add(cO)
 f.Controls.Add(lwcc)
 f.Show()
